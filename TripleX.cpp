19bd8232da0446120e9fb22a3432a4d731af30d3
Alan Harlow <harlowashx00@gmail.com>
2019-07-10 07:27:46 -0400

    Moved intro into its own function. Added level difficulty and general code cleanup.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+void PrintIntroduction(int);
+bool PlayGame(int);
 
 int main() {
-    // Printng welcome message to the terminal
-    std::cout << "You are a secret agent breaking into a secure server room..." << std::endl;
-    std::cout << "Enter the correct code to continue..." << std::endl;
+    int LevelDifficulty = 1;
 
+    while (true) {
+
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete) {
+            ++LevelDifficulty;
+        }
+        
+    }
+    return 0;
+}
+
+bool PlayGame(int Difficulty) {
+    PrintIntroduction(Difficulty);
+    
     // Declare 3 number code
     const int CodeA = 4;
     const int CodeB = 3;
@@ -14,19 +31,29 @@ int main() {
     const int CodeSum = CodeA + CodeB + CodeC;
 
     // Print sum and product to terminal
-    std::cout << std::endl;
-    std::cout << "There are three numbers in the code" << std::endl;
-    std::cout << "The code multiplies to: " << CodeProduct << std::endl;
-    std::cout << "The code adds up to: " << CodeSum << std::endl;
+    std::cout << "\nThere are three numbers in the code" << std::endl;
+    std::cout << "+ The code adds up to: " << CodeSum << std::endl;
+    std::cout << "+ The code multiplies to: " << CodeProduct << std::endl;
 
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     // Totals of guess used for testing for correct answer
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
-    return 0;
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
+        std::cout << "You win!" << std::endl;
+        return true;
+    }
+    else {
+        std::cout << "You lose!" << std::endl;
+        return false;
+    }
+}
+
+void PrintIntroduction(int Difficulty) {
+    // Printng welcome message to the terminal
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nEnter the correct code to continue..." << std::endl;
 }
